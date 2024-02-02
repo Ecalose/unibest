a67977bc663e9dc91a3f86c2b3c047f3d0b9ebe8
菲鸽 <1020103647@qq.com>
2024-02-02 10:43:09 +0800

    build: h5-->unibest

--- shell/deploy-gitee.sh
+++ shell/deploy-gitee.sh
@@ -1,4 +1,5 @@
-if test -d './h5'; then
-  rm -rf './h5'
+# 改为放到仓库名称下，这样代码可以不用改，同时兼容 github + gitee
+if test -d './unibest'; then
+  rm -rf './unibest'
 fi
-mv ./dist/build/h5 ./h5
+mv ./dist/build/h5 ./unibest
