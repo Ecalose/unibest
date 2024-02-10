79a9d7ae7bb7618ae061dc08ded8174370bfc611
菲鸽 <1020103647@qq.com>
2024-02-11 00:10:09 +0800

    build: pnpm build:h5

---
+++ unibest/assets/foo.acdca09f.js
@@ -0,0 +1,1 @@
+import{h as o}from"./http.12eaae47.js";const a=a=>o({url:"/foo",method:"GET",query:{name:a}}),e=a=>o({url:"/foo",method:"POST",query:{name:a},data:{name:a}});export{a as g,e as p};
---
+++ unibest/assets/index.b39ba5b3.js
@@ -0,0 +1,1 @@
+import{P as t,a1 as a,J as s}from"./index-4b101180.js";import{p as e}from"./pages.06d55f38.js";const r=()=>{if(!Object.keys(e).includes("tabBar"))return!1;const a=t().at(-1).route;return!!e.tabBar.list.find((t=>t.pagePath===a))},n=()=>{s({title:"i18n 测试",content:a("app.name")})};export{r as g,n as t};
